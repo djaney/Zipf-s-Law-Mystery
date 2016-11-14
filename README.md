4631b8cc9155d2fb7ec3e44e0ec5ab94696d3380
Djane Rey Mabelin <thedjaney@gmail.com>
2016-11-15 03:33:04 +0800

    code stuff

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Zif's Law Mystery
 Back in 1949, the linguist George Zipf noticed something odd about how often people use words in a given language. He found that a small number of words are used all the time, while the vast majority are used very rarely. If he ranked the words in order of popularity, a striking pattern emerged. The number one ranked word was always used twice as often as the second rank word, and three times as often as the third rank. He called this a rank vs. frequency rule, and found that it could also be used to describe income distributions in any given country, with the richest person making twice as much money as the next richest, and so forth.
+
+## Command
+Read a TXT file and the program will try to predict how many times the nth word occurred using Zif's Law.
+```
+    node zipfs books\lord-of-the-rings.txt 3245
+```
+Output:
+```
+According to Zif's Law, the word "interrupted" should occur 10 times. It occurred 12 times. Off the mark by 2.
+```
---
+++ zipfs.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+
+if(4 > process.argv.length){
+    console.log("node zipfs <filename> <rank to guess>");
+    process.exit();
+}
+
+
+var filename = process.argv[2];
+var guess = process.argv[3];
+
+fs.readFile(filename, 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    var rankings = rankWords(data);
+
+    if(guess >= rankings.length){
+        console.log("try a number smaller than " + rankings.length);
+        process.exit();
+    }
+
+    printPrediction(rankings, guess);
+});
+function printPrediction(rankings, i){
+    var theWord = rankings[i].word;
+    var prediction = (rankings[0].count/i).toFixed(0);
+    var actual = rankings[i].count;
+    var diff = Math.abs(prediction - actual);
+        console.log("According to Zif's Law, the word \""+theWord+"\" should occur "+prediction+" times. It occurred "+actual+" times. Off the mark by "+diff+".");
+}
+function rankWords(book){
+    var words = book.replace(/\s+/g, ' ').split(" ");
+    var ranks = {};
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i].trim().replace(/\W+/g,'');
+        if(ranks.hasOwnProperty(word)){
+            ranks[word]++;
+        }else{
+            ranks[word] = 1;
+        }
+    }
+    var ranksArray = [];
+    for (var r in ranks) {
+        if (ranks.hasOwnProperty(r)) {
+            ranksArray.push({word: r, count: ranks[r]});
+        }
+    }
+
+    return ranksArray.sort(function(x,y){
+        return y.count - x.count;
+    });
+}
